d07e3a37f3dc4d61ae556c7b7449e1d50fe9a796
ClementBLV <clement.beliveau@imt-atlantique.net>
2024-03-18 18:06:04 +0100

    Feature : add a loop in the script

--- script.sh
+++ script.sh
@@ -6,11 +6,13 @@ python3 dataset.py 	--test_content_dir $BASE'/data/content'\
 			--test_style_dir $BASE'/data/style'
 echo "---------------------------------------"
 echo "Style transfer in process"
-python test.py --content $BASE'/data/content' \
-		--style $BASE'/data/style' \
+for i in 1 2 3 4 5 6 7 8 9 10 ;do
+	python test.py --content $BASE'/data/content_resized/dog.jpeg' \
+		--style $BASE'/data/style_resized/wave.jpg' \
 		--output_name $BASE'/output'\
 		--model_state_path $BASE'/model_state.pth'\
-		--n_cluster "9"\
+		--n_cluster $i\
 		--gpu "-1"
+done
 echo "---------------------------------------"
 echo "Done !"
