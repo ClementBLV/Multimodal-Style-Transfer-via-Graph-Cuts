b56e095f10181267d5a4bd91bc8d0692535d12c4
ClementBLV <clement.beliveau@imt-atlantique.net>
2024-03-18 10:11:11 +0100

    Bug Fix : file saving - good path in output

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import torch
 from torchvision import transforms
 from torchvision.utils import save_image
 from model import Model
+import numpy as np 
 
 trans = transforms.Compose([transforms.ToTensor()])
 
@@ -30,12 +31,39 @@ def main():
     parser.add_argument('--model_state_path', type=str, default='model_state.pth',
                         help='pretrained model state')
 
+
     args = parser.parse_args()
     #########
-    content_dir_resized = args.content + '_resized'
-    style_dir_resized = args.style + '_resized'
-    assert (os.path.exists(content_dir_resized)) , 'The content must be resized before to avoid issues'
-    assert (os.path.exists(style_dir_resized)), 'The style must be resized before to avoid issues'
+    if "." not in  args.content:
+        print('|_ Image not specified - random pick')
+        if '_resized' not in args.content:
+            content_dir_resized = args.content + '_resized'
+        else : 
+            content_dir_resized = args.content
+        assert (os.path.exists(content_dir_resized)) , 'The content must be resized before to avoid issues'
+        random_pick =np.random.choice(os.listdir(content_dir_resized), 1)[0]
+        content_dir_resized = content_dir_resized + '/'+ random_pick
+        print(f'|__ Image picked - {random_pick}')
+    else :
+        assert '_resized' in  args.content, "Image must be resized (or in a resized directory)"
+        print('|_ Image specified and resized') 
+        content_dir_resized =args.content
+
+    if "." not in args.style:
+        print('|_ Image not specified - random pick')
+        if '_resized' not in args.style:
+            style_dir_resized = args.style + '_resized'
+        else : 
+            style_dir_resized = args.style    
+        assert (os.path.exists(style_dir_resized)), 'The style must be resized before to avoid issues'
+        random_pick = np.random.choice(os.listdir(style_dir_resized), 1)[0]
+        style_dir_resized = style_dir_resized +'/'+ random_pick 
+        print(f'|__ Image picked - {random_pick}')
+
+    else : 
+        assert '_resized' in  args.style, "Image must be resized (or in a resized directory)"
+        style_dir_resized = args.style 
+        print('|_ Image specified and resized') 
 
     #########
     # set device on GPU if available, else CPU
@@ -53,24 +81,28 @@ def main():
                   max_cycles=args.max_cycles)
     if args.model_state_path is not None:
         model.load_state_dict(torch.load(args.model_state_path, map_location=lambda storage, loc: storage))
-        print(f'{args.model_state_path} loaded')
+        #print(f'{args.model_state_path} loaded')
+        print('|_ model loaded')
     model = model.to(device)
 
     c = Image.open(content_dir_resized)
     s = Image.open(style_dir_resized)
     c_tensor = trans(c).unsqueeze(0).to(device)
     s_tensor = trans(s).unsqueeze(0).to(device)
-
     with torch.no_grad():
         out = model.generate(c_tensor, s_tensor).to('cpu')
 
-    if args.output_name is None:
-        c_name = os.path.splitext(os.path.basename(content_dir_resized))[0]
-        s_name = os.path.splitext(os.path.basename(style_dir_resized))[0]
-        args.output_name = f'{c_name}_{s_name}'
+    c_name = os.path.splitext(os.path.basename(content_dir_resized))[0]
+    s_name = os.path.splitext(os.path.basename(style_dir_resized))[0]
 
-    save_image(out, f'{args.output_name}.jpg', nrow=1)
-    o = Image.open(f'{args.output_name}.jpg')
+    if args.output_name is None:
+        output_name = f'/{c_name}_{s_name}'
+    else:
+        output_name = f'{args.output_name}/{c_name}_{s_name}'
+
+    save_image(out, f'{output_name}.jpg', nrow=1)
+    
+    o = Image.open(f'{output_name}.jpg')
 
     demo = Image.new('RGB', (c.width * 2, c.height))
     o = o.resize(c.size)
@@ -79,12 +111,12 @@ def main():
     demo.paste(c, (0, 0))
     demo.paste(o, (c.width, 0))
     demo.paste(s, (c.width, c.height - s.height))
-    demo.save(f'{args.output_name}_style_transfer_demo.jpg', quality=95)
+    demo.save(f'{output_name}_style_transfer_demo.jpg', quality=95)
 
     o.paste(s,  (0, o.height - s.height))
-    o.save(f'{args.output_name}_with_style_image.jpg', quality=95)
-
-    print(f'result saved into files starting with {args.output_name}')
+    o.save(f'{output_name}_with_style_image.jpg', quality=95)
+    
+    print(f'|_ result saved {output_name}')
 
 
 if __name__ == '__main__':
